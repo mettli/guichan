aafaa78ca772e382acb611af9cc739c7c909c9c4
jansem <jansem@9b4f5164-5a2d-0410-9de4-b3417c078dbe>
2004-02-18 23:15:49 +0000

    sdl.hpp for using the SDL drivers.

---
+++ trunk/include/guichan/sdl.hpp
@@ -0,0 +1,8 @@
+#ifndef GCN_SDL_HPP
+#define GCN_SDL_HPP
+
+#include <guichan/sdl/sdlgraphics.hpp>
+#include <guichan/sdl/sdlimage.hpp>
+#include <guichan/sdl/sdlinput.hpp>
+
+#endif // end GCN_SDL_HPP
