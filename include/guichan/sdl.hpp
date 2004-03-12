315858064327c6bd3f60cec8c6928e5de0462c0e
jansem <jansem@9b4f5164-5a2d-0410-9de4-b3417c078dbe>
2004-03-12 22:20:41 +0000

    Removed sdlimage.hpp added sdlimageloader.hpp

--- include/guichan/sdl.hpp
+++ include/guichan/sdl.hpp
@@ -2,7 +2,7 @@
 #define GCN_SDL_HPP
 
 #include <guichan/sdl/sdlgraphics.hpp>
-#include <guichan/sdl/sdlimage.hpp>
+#include <guichan/sdl/sdlimageloader.hpp>
 #include <guichan/sdl/sdlinput.hpp>
 
 #endif // end GCN_SDL_HPP
