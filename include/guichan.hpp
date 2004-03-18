afb9a08b6118385c24efe3e7b0f1ac6098cbb2e4
jansem <jansem@9b4f5164-5a2d-0410-9de4-b3417c078dbe>
2004-03-18 21:23:20 +0000

    Added icon.hpp

--- include/guichan.hpp
+++ include/guichan.hpp
@@ -3,11 +3,12 @@
 
 // #include "guichan/actionlistener.hpp"
 #include <guichan/color.hpp>
-#include "guichan/container.hpp"
+#include <guichan/container.hpp>
+#include <guichan/exception.hpp>
 // #include "guichan/focushandler.hpp"
 // #include "guichan/font.hpp"
 #include <guichan/graphics.hpp>
-#include "guichan/gui.hpp"
+#include <guichan/gui.hpp>
 #include <guichan/image.hpp>
 #include <guichan/imageloader.hpp>
 // #include "guichan/imagemousetype.hpp"
@@ -20,6 +21,8 @@
 // #include "guichan/mousetype.hpp"
 #include <guichan/rectangle.hpp>
 #include <guichan/cliprectangle.hpp>
-#include "guichan/widget.hpp"
+#include <guichan/widget.hpp>
+
+#include <guichan/widgets/icon.hpp>
 
 #endif // end GCN_GUICHAN_HPP
