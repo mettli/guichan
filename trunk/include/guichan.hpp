ca3fc283704f44e44613d18cbb627842d6dce11b
jansem <jansem@9b4f5164-5a2d-0410-9de4-b3417c078dbe>
2004-03-21 20:35:04 +0000

    Added imagefont.hpp

--- trunk/include/guichan.hpp
+++ trunk/include/guichan.hpp
@@ -2,14 +2,15 @@
 #define GCN_GUICHAN_HPP
 
 // #include "guichan/actionlistener.hpp"
+#include <guichan/graphics.hpp>
 #include <guichan/color.hpp>
 #include <guichan/container.hpp>
 #include <guichan/exception.hpp>
 // #include "guichan/focushandler.hpp"
-// #include "guichan/font.hpp"
-#include <guichan/graphics.hpp>
+#include "guichan/font.hpp"
 #include <guichan/gui.hpp>
 #include <guichan/image.hpp>
+#include "guichan/imagefont.hpp"
 #include <guichan/imageloader.hpp>
 // #include "guichan/imagemousetype.hpp"
 #include <guichan/input.hpp>
