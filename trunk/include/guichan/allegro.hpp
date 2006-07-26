5d24a8d49d318479138693f381d770b865cc5b5c
yakslem <yakslem@9b4f5164-5a2d-0410-9de4-b3417c078dbe>
2006-07-26 11:21:19 +0000

    Inclusion of AllegroImage.hpp has been added.

--- trunk/include/guichan/allegro.hpp
+++ trunk/include/guichan/allegro.hpp
@@ -58,6 +58,7 @@
 #define GCN_ALLEGRO_HPP
 
 #include <guichan/allegro/allegrographics.hpp>
+#include <guichan/allegro/allegroimage.hpp>
 #include <guichan/allegro/allegroimageloader.hpp>
 #include <guichan/allegro/allegroinput.hpp>
 
